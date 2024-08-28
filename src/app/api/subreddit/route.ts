4b89d30a5f73b799999f6f149e98885992694315
Harshal <harshalpatel2193@gmail.com>
2024-08-28 10:59:16 +0530

    removed logs

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -6,24 +6,22 @@ import { z } from "zod"
 export async function POST(req: Request) {
     try {
         const session = await getAuthSession()
-        console.log("1");
+
         if (!session?.user) {
             return new Response('Unauthorized', { status: 401 })
         }
-        console.log("2");
-        // console.log("req", session);
 
         const body = await req.json()
-        console.log("2.1");
+
         const { name } = Subredditvalidator.parse(body)
-        console.log("2.2");
+
         const subredditexists = await db.subreddit.findFirst({
             where: {
                 name,
             },
 
         })
-        console.log("3");
+
         if (subredditexists) {
             return new Response('Subreddit already Exists', { status: 409 })
         }
@@ -33,20 +31,20 @@ export async function POST(req: Request) {
                 creatorId: session.user.id,
             }
         })
-        console.log("4");
+
         await db.subscription.create({
             data: {
                 userId: session.user.id,
                 subredditId: subreddit.id,
             }
         })
-        console.log("5");
+
         return new Response(subreddit.name)
     } catch (error) {
         if (error instanceof z.ZodError) {
             return new Response(error.message, { status: 422 })
         }
-        console.log("Error :: ", error);
+        console.error("Error :: ", error);
 
         return new Response('Could not create subreddit', { status: 500 })
 
