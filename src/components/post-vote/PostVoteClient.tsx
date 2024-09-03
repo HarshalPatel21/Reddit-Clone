ed3f2625b2904fd80369891a58ed4cab2a2dc9c0
sanstuti jain <sanstuti.jain993@gmail.com>
2024-09-03 20:40:49 +0530

    upvote error

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -7,6 +7,9 @@ import { usePrevious } from "@mantine/hooks";
 import { Button } from "../ui/Button";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { PostVoteRequest } from "@/lib/validators/vote";
+import axios from "axios";
 
 interface PostVoteClientProps {
   postId: string;
@@ -28,8 +31,22 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
     setCurrentVote(initialVote)
   },[initialVote])
 
+  const{mutate:vote}=useMutation({
+    mutationFn: async (voteType:VoteType)=>{
+      const payload : PostVoteRequest={
+        postId,
+        voteType
+
+      }
+      await axios.patch('/api/subreddit/post/vote',payload)
+
+    },
+
+
+  })
+
   return <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm-w-20 pb-4 sm:pb-0">
-    <Button size='sm' variant='ghost' aria-label="upvote">
+    <Button onClick={()=>vote('UP')} size='sm' variant='ghost' aria-label="upvote">
     <ArrowBigUp
     className={cn('h-5 w-5 text-zinc-700',{
       "text-emerald-500 fill-emerald-500": currentVote === 'UP',
@@ -38,7 +55,7 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
     <p className="text-center py-2 font-medium text-sm text-zinc-900">
       {votesAmt}
     </p>
-    <Button size='sm' variant='ghost' aria-label="downvote">
+    <Button onClick={()=>vote('DOWN')} size='sm' variant='ghost' aria-label="downvote">
     <ArrowBigDown
     className={cn('h-5 w-5 text-zinc-700',{
       "text-red-500 fill-emerald-500": currentVote === 'DOWN',
